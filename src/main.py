a9c9700a09f751e5db2ef216eefb36ad21dd9852
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-05-15 20:13:49 +0200

    Co-authored-by: JoakimSundman <JoakimSundman@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -14,6 +14,7 @@ load_dotenv("token.env")  # Load from .env
 token = os.getenv("GITHUB_TOKEN")
 
 headers = {
+	"Accept": "application/vnd.github+json",
 	"Authorization": f"token {token}",
 	"User-Agent": "RegexCrawler/1.0",
 	"X-GitHub-Api-Version": "2022-11-28"
@@ -27,7 +28,7 @@ def get_repos():
 		return
 	
 	data = json.loads(r.text)
-	urls = [item["html_url"] for item in data["items"]]
+	urls = [item["full_name"] for item in data["items"]]
 	with open("search_results.txt", "w") as f:
 		for url in urls:
 			f.write(url + "\n")
@@ -35,12 +36,35 @@ def get_repos():
 	
 
 def get_files():
+    with open("search_results.txt", "r") as f:
+        repo_list = [line.strip() for line in f if line.strip()]
+
+    # Pattern to search in each repo
+    search_terms = '"match(" OR "test(" OR "regex(" OR "RegExp(" OR "exec("'
+
+    for repo in repo_list:
+        query = f'{search_terms} language:JavaScript repo:{repo}'
+
+        params = {
+            'q': query,
+            'per_page': 10
+        }
+
+        print(f"Searching in {repo}...")
+
+        r = requests.get("https://api.github.com/search/code", headers=headers, params=params)
+        print(r.url)
+        if r.status_code == 200:
+            results = json.loads(r.text)
+            print(results)
+
+"""def get_files():
 	with open("search_results.txt", "r") as f:
 		for link in f:
 			r = requests.get("https://github.com/search?q=repo%3A" + link + repo_filter, headers=headers)
 			with open("repo_text_test.txt", "w") as f:
 				f.write(r.text)
-			break
+			break"""
 
 if __name__ == "__main__":
 	get_repos()
