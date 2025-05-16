1a5617ba7401dc8af400956b2df6e1f1f971673a
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-05-16 09:51:45 +0200

    Extract regexes from files

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import json
 from urllib.parse import quote
 import time
+import re
 
 parser = 'html.parser'
 
@@ -35,54 +36,37 @@ def get_repos():
 		for url in urls:
 			f.write(url + "\n")
 
-def get_files():
+def get_regexes():
 	base = 'https://api.github.com/search/code?q='
-	#repo = ' repo:webpack/webpack'
-	filter = '.test OR .match OR RegExp OR .exec in:file language:JavaScript'
+	# TODO Iterate through filters to get more results
+	search_filter = '.match in:file language:JavaScript'
 	repo_list = []
 	with open("search_results.txt", "r") as f:
 		repo_list = [line.strip() for line in f if line.strip()]
 	
+	content_headers = {
+		"Accept": "application/vnd.github.raw+json",
+		"Authorization": f"token {token}",
+		"User-Agent": "RegexCrawler/1.0",
+		"X-GitHub-Api-Version": "2022-11-28"
+	}	
+
 	for repo in repo_list:
-		url = base + quote(filter) + ' repo:' + repo
-		print(url)
-		name = repo.replace("/", "") + ".json"
-		with open("outputs/" + name, "w") as f:
-			r = requests.get(url, headers=headers)
-			time.sleep(6.5)
-			f.write(r.text)
-
-"""with open("search_results.txt", "r") as f:
-        repo_list = [line.strip() for line in f if line.strip()]
-
-    # Pattern to search in each repo
-    search_terms = '"match(" OR "test(" OR "regex(" OR "RegExp(" OR "exec("'
-
-    for repo in repo_list:
-        query = f'{search_terms} language:JavaScript repo:{repo}'
-
-        params = {
-            'q': query,
-            'per_page': 10
-        }
-
-        print(f"Searching in {repo}...")
-
-        r = requests.get("https://api.github.com/search/code", headers=headers, params=params)
-        print(r.url)
-        if r.status_code == 200:
-            results = json.loads(r.text)
-            print(results)
-"""
-
-"""def get_files():
-	with open("search_results.txt", "r") as f:
-		for link in f:
-			r = requests.get("https://github.com/search?q=repo%3A" + link + repo_filter, headers=headers)
-			with open("repo_text_test.txt", "w") as f:
-				f.write(r.text)
-			break"""
+		url = base + quote(search_filter) + ' repo:' + repo
+		r = requests.get(url, headers=headers)
+		time.sleep(6.5)
+		data = json.loads(r.text)
+		paths = [item["path"] for item in data["items"]]
+		for path in paths:
+			get_req = "https://api.github.com/repos/" + repo + "/contents/" + path
+			content_req = requests.get(get_req, headers=content_headers)
+			regex = r'(?: /(?!\*\*|\/).*?/)|(?:\"/(?!\*\*|\/).*?/\" )|(?:\'/(?!\*\*|\/).*?/\')'
+			regex_list = re.findall(regex, content_req.text)
+			if len(regex_list) != 0: #TODO Log instead of print
+				print(path)
+				print(regex_list)
+		return # TODO Remove
 
 if __name__ == "__main__":
 	#get_repos()
-	get_files()
+	get_regexes()
