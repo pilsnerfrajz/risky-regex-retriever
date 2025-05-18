a57bae71512fdc9b3cc734cec980bb3403aa33b6
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-05-18 14:43:29 +0200

    Add missing slash

--- src/main.py
+++ src/main.py
@@ -85,7 +85,7 @@ def get_regexes():
 
 						if len(valid_regexes) != 0:
 							print("\tFound unique and valid regexes")
-							f.write("Repo " + re_function + " " + repo + path + "\n")
+							f.write("Repo " + re_function + " " + repo + "/" + path + "\n")
 							f.write("\n".join(valid_regexes) + "\n" + "\n")
 					
 				except KeyError:
