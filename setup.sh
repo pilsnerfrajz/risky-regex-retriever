140dc8089e18f29d39654fef4c31c7418e3c890a
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-05-20 20:57:47 +0200

    Make debugging easier if error during setup occurs

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ echo "Installing npm..."
 sudo snap install node --channel=23/stable --classic || { echo "Install failed"; exit 1; }
 
 echo "Cloning vuln-regex-detector..."
-git clone https://github.com/davisjam/vuln-regex-detector.git || { echo "Clone failed"; exit 1; }
+git clone https://github.com/davisjam/vuln-regex-detector.git
 
 echo "Changing directory..."
-cd vuln-regex-detector
+cd vuln-regex-detector || { exit 1; }
 
-echo "Replacing invalid directory..."
+echo "Replacing invalid directory 'src/cache/client' with 'src/cache/client/npm' in 'configure'..."
 sed -i 's|src/cache/client/|src/cache/client/npm/|g' configure || { echo "Replacement failed"; exit 1; }
 
 echo "Setting environment variable..."
@@ -22,7 +22,7 @@ echo "Running configuration script..."
 echo "\nCreate a virtual environment and install dependencies with:"
 echo "    python3 -m venv env/redos"
 echo "    source env/redos/bin/activate"
-echo "    pip3 install -r requirements.py"
+echo "    pip3 install -r requirements.txt"
 
 echo "Run scanner with:"
 echo "    python3 src/main.py"
